d8885505adf521194fba1645ebd4f0b5c50e5e60
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-06 10:31:29 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -20,7 +20,6 @@ class CommandLineInterface
 
   def add_attributes_to_students
     Student.all.each do |student|
-      binding.pry
       attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
       student.add_student_attributes(attributes)
       end
